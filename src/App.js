c3ba764ff1c68f45f36af9b5b1b588e5bcf02b02
Greig <greigcolliar@gmail.com>
2021-08-24 10:14:06 +0100

    test

--- src/App.js
+++ src/App.js
@@ -2,19 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import About from './About';
+import Home from './Home';
 import {useEffect, useState} from 'react';
 
 function App() {
 
   const [pokemon, setPokemon] = useState();
+  // when dom loads use useEffect to grab pokemon data from api
   useEffect(()=> {
     fetch("https://pokeapi.co/api/v2/pokemon?offset=0")
+      .then((res) => res.json())
+      .then((data) => {
+        // map the results and use idx to grab the information
+        const results = data.results.map((pokemon, idx) => {
+          return { ...pokemon, idx: idx + 1};
+        });
+        // set pokemon to results
+        setPokemon({ ...data, results});
+      });
   })
 
   return (
     <Router>
       <div className="App">
         <h1 className="text-2xl">It's working now???</h1>
+        <home
       </div>
 
       <Switch>
